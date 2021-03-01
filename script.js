fc1739cabe34efc9cbfa9b290db6732f348f4af0
Nicholas Chumney <nicholas.chumney@outlook.com>
2021-03-01 18:18:19 -0500

    created grid

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("script is running");
+const rows = 10;
+const cols = 10;
+
+const board = new Array(rows).fill(new Array(cols).fill(false));
+
+function createTable() {
+  const table = document.getElementById("board");
+  const tbody = document.createElement("tbody");
+
+  for (let i = 0; i < rows; i++) {
+    const tr = document.createElement("tr");
+
+    for (let j = 0; j < cols; j++) {
+      const td = document.createElement("td");
+      td.setAttribute("id", `${i}-${j}`);
+      td.addEventListener("click", (e) => {
+        console.log("clicked on " + e.target.id);
+      });
+
+      tr.appendChild(td);
+    }
+
+    tbody.appendChild(tr);
+  }
+
+  table.appendChild(tbody);
+}
+
+createTable();
